8018e9ec5b1c337d3ecc50b3864327bb588332b0
Nils <nils@mw-008.local>
2020-02-04 15:11:20 +0100

    scaffold out navbar and login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Start from './components/Start';
 import 'semantic-ui-css/semantic.min.css';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <Start />
+      <Login />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu } from 'semantic-ui-react';
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState({});
+
+  const handleItemClick = (e, { name }) => setActiveItem({ activeItem: name });
+
+
+  return (
+    <Menu>
+      <Menu.Item
+        name="Home"
+        active={activeItem === 'home'}
+        content="Home"
+        onClick={handleItemClick}
+      />
+
+      <Menu.Item
+        name="Login"
+        active={activeItem === 'Login'}
+        content="Login"
+        onClick={handleItemClick}
+      />
+
+      <Menu.Item
+        name="Logout"
+        active={activeItem === 'Logout'}
+        content="Logout"
+        onClick={handleItemClick}
+      />
+    </Menu>
+  );
+};
+
 
 export default Navbar;
