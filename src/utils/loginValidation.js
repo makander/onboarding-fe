adb1fd4579c2ab55348ef39bbaf084ec0381b44a
Nils Makander <makander@gmail.com>
2020-04-30 18:23:49 +0200

    remove cruft files

--- src/utils/loginValidation.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-export const loginSchema = yup.object().shape({
-  email: yup.string().email('Must be a valid email address'),
-  password: yup.string().url('Must be a valid url'),
-});
-
-export default loginSchema;
